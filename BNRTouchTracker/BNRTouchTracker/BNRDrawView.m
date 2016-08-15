af4e870a27f840508253d274c960f575e4f08b1b
charlieusc <yangjialinusc@gmail.com>
2016-08-15 12:20:13 -0700

    Correct mysterious lines and added feature changing size according to drawing speed.

--- BNRTouchTracker/BNRTouchTracker/BNRDrawView.m
+++ BNRTouchTracker/BNRTouchTracker/BNRDrawView.m
@@ -9,10 +9,14 @@
 #import "BNRDrawView.h"
 #import "BNRLine.h"
 
-@interface BNRDrawView()
+@interface BNRDrawView() <UIGestureRecognizerDelegate>
 
 @property(nonatomic, strong) NSMutableDictionary *linesInProgress;
 @property(nonatomic, strong) NSMutableArray *finishedLines;
+@property(nonatomic, weak) BNRLine *selectedLine;
+@property(nonatomic, weak) BNRLine *pressSelectedLine;
+@property(nonatomic, strong) UIPanGestureRecognizer *moveRecognizer;
+@property(nonatomic, strong) NSMutableArray *movingSpeed;
 
 @end
 
@@ -26,14 +30,116 @@
         self.finishedLines = [[NSMutableArray alloc] init];
         self.backgroundColor = [UIColor grayColor];
         self.multipleTouchEnabled = YES;
+        
+        UITapGestureRecognizer *doubleTabRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTab:)];
+        doubleTabRecognizer.numberOfTapsRequired = 2;
+        doubleTabRecognizer.delaysTouchesBegan = YES;
+        [self addGestureRecognizer:doubleTabRecognizer];
+        
+        UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
+        tapRecognizer.delaysTouchesBegan = YES;
+        [tapRecognizer requireGestureRecognizerToFail:doubleTabRecognizer];
+        [self addGestureRecognizer:tapRecognizer];
+        
+        UILongPressGestureRecognizer *pressRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPress:)];
+        [self addGestureRecognizer:pressRecognizer];
+        
+        self.moveRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveLine:)];
+        self.moveRecognizer.delegate = self;
+        self.moveRecognizer.cancelsTouchesInView = NO;
+        [self addGestureRecognizer:self.moveRecognizer];
+        
+        self.movingSpeed = [[NSMutableArray alloc] init];
     }
     return self;
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(nonnull UIGestureRecognizer *)otherGestureRecognizer
+{
+    if(gestureRecognizer == self.moveRecognizer){
+        return YES;
+    }
+    return NO;
+}
+
+- (void)moveLine:(UIPanGestureRecognizer *)gr
+{
+    if(self.selectedLine){
+        self.selectedLine = nil;
+        [[UIMenuController sharedMenuController] setMenuVisible:NO animated:YES];
+    }
+    CGPoint velocity = [gr velocityInView:self];
+    float speed = sqrtf(powf(velocity.x, 2)+powf(velocity.y, 2));
+    NSLog(@"Speed:%.2f", speed);
+    [self.movingSpeed addObject:[NSNumber numberWithFloat:speed]];
+    if(gr.state == UIGestureRecognizerStateChanged){
+        CGPoint translation = [gr translationInView:self];
+        CGPoint begin = self.pressSelectedLine.begin;
+        CGPoint end = self.pressSelectedLine.end;
+        begin.x += translation.x;
+        begin.y += translation.y;
+        end.x += translation.x;
+        end.y += translation.y;
+        self.pressSelectedLine.begin = begin;
+        self.pressSelectedLine.end = end;
+        [self setNeedsDisplay];
+        [gr setTranslation:CGPointZero inView:self];
+    }
+}
+
+- (void)longPress:(UIGestureRecognizer *)gr
+{
+    if(gr.state == UIGestureRecognizerStateBegan){
+        CGPoint point = [gr locationInView:self];
+        self.pressSelectedLine = [self lineAtPoint:point];
+        
+        if(self.pressSelectedLine){
+            [self.linesInProgress removeAllObjects];
+        }
+    }else if(gr.state == UIGestureRecognizerStateEnded){
+        self.pressSelectedLine = nil;
+    }
+    [self setNeedsDisplay];
+}
+
+- (BOOL)canBecomeFirstResponder
+{
+    return YES;
+}
+
+- (void)tap:(UIGestureRecognizer *)gr
+{
+    //NSLog(@"Recognized tap");
+    
+    CGPoint point = [gr locationInView:self];
+    self.selectedLine = [self lineAtPoint:point];
+    if(self.selectedLine){
+        [self becomeFirstResponder];
+        UIMenuController *menu = [UIMenuController sharedMenuController];
+        
+        UIMenuItem *deleteItem = [[UIMenuItem alloc] initWithTitle:@"Delete" action:@selector(deleteLine:)];
+        menu.menuItems = @[deleteItem];
+        [menu setTargetRect:CGRectMake(point.x, point.y, 2, 2) inView:self];
+        [menu setMenuVisible:YES animated:YES];
+    }else{
+        [[UIMenuController sharedMenuController] setMenuVisible:NO animated:YES];
+    }
+    
+    [self setNeedsDisplay];
+}
+
+- (void)doubleTab:(UIGestureRecognizer *)gr
+{
+    //NSLog(@"Recognizer Double Tap");
+    [self.linesInProgress removeAllObjects];
+    [self.finishedLines removeAllObjects];
+    [self setNeedsDisplay];
 }
 
 - (void)strokeLine:(BNRLine *)line
 {
     UIBezierPath *bp = [UIBezierPath bezierPath];
-    bp.lineWidth = 10;
+    bp.lineWidth = line.lineWidth;
     bp.lineCapStyle = kCGLineCapRound;
     
     [bp moveToPoint:line.begin];
@@ -52,12 +158,20 @@
     for(NSValue *key in self.linesInProgress){
         [self strokeLine:self.linesInProgress[key]];
     }
+    if(self.selectedLine){
+        [[UIColor greenColor] set];
+        [self strokeLine:self.selectedLine];
+    }
+    if(self.pressSelectedLine){
+        [[UIColor whiteColor] set];
+        [self strokeLine:self.pressSelectedLine];
+    }
     
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    NSLog(@"%@", NSStringFromSelector(_cmd));
+    //NSLog(@"%@", NSStringFromSelector(_cmd));
     
     for(UITouch *t in touches){
         CGPoint location = [t locationInView:self];
@@ -65,6 +179,7 @@
         BNRLine *line = [[BNRLine alloc] init];
         line.begin = location;
         line.end = location;
+        line.lineWidth = 10;
         
         NSValue *key = [NSValue valueWithNonretainedObject:t];
         self.linesInProgress[key] = line;
@@ -75,7 +190,7 @@
 
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    NSLog(@"%@", NSStringFromSelector(_cmd));
+    //NSLog(@"%@", NSStringFromSelector(_cmd));
     
     for(UITouch *t in touches) {
         NSValue *key = [NSValue valueWithNonretainedObject:t];
@@ -88,11 +203,17 @@
 
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    NSLog(@"%@", NSStringFromSelector(_cmd));
+    //NSLog(@"%@", NSStringFromSelector(_cmd));
     for(UITouch *t in touches){
         NSValue *key = [NSValue valueWithNonretainedObject:t];
         BNRLine *line = self.linesInProgress[key];
-        
+        float aveSpeed = 0;
+        for(NSNumber *speed in self.movingSpeed){
+            aveSpeed+=[speed floatValue];
+        }
+        aveSpeed /=[self.movingSpeed count];
+        line.lineWidth = 2 / aveSpeed * 1000;
+        [self.movingSpeed removeAllObjects];
         [self.finishedLines addObject:line];
         [self.linesInProgress removeObjectForKey:key];
     }
@@ -101,7 +222,7 @@
 
 - (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    NSLog(@"%@", NSStringFromSelector(_cmd));
+    //NSLog(@"%@", NSStringFromSelector(_cmd));
     
     for(UITouch *t in touches){
         NSValue *key = [NSValue valueWithNonretainedObject:t];
@@ -111,8 +232,31 @@
     [self setNeedsDisplay];
 }
 
+- (BNRLine *)lineAtPoint:(CGPoint)p
+{
+    for(BNRLine *l in self.finishedLines){
+        CGPoint start = l.begin;
+        CGPoint end = l.end;
+        
+        for(float t=0.0; t<=1.0; t+=0.05){
+            float x = start.x + t * (end.x - start.x);
+            float y = start.y + t * (end.y - start.y);
+            
+            if(hypot(x - p.x, y - p.y) < 20.0){
+                return l;
+            }
+        }
+    }
+    
+    return nil;
+}
 
 
+- (void)deleteLine:(id)sender
+{
+    [self.finishedLines removeObject:self.selectedLine];
+    [self setNeedsDisplay];
+}
 
 
 
